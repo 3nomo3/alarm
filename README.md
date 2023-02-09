07347df82179c0004bf462ccea1c8d1d4fe758f6
3nomo3 <solock3369@gmail.com>
2023-02-09 22:07:40 +0900

    created two files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# nomodir
+# 使い方
+
+alarm_3.py -> python3 alarm_3.py とすれば、3秒経過後helloと表示する。
+
+alarm_x.py -> python3 alarm_x.py 10 とすれば、10秒経過後helloと表示する。
---
+++ alarm_x.py
@@ -0,0 +1,11 @@
+from time import sleep
+import sys
+
+args = sys.argv
+
+#print(args[0])
+#print(args[1])
+#print(args[2])
+
+sleep(int(args[1]))
+print("hello")
